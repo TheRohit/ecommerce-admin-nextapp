c0dee8fa52ea212b2b2689e989d452bc2d44d9db
TheRohit <rohitmeshram000@gmail.com>
2023-05-26 23:49:25 +0530

    category post working

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,3 +1,6 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Category } from "@/models/Category";
+
 export default async function handle(req,res){
     const {method} = req;
     await mongooseConnect();
@@ -6,6 +9,8 @@ export default async function handle(req,res){
         const {name}= req.body; 
         //same as const name=req.body.name
        
+       const categoryDoc= await Category.create({name});
+       res.json(categoryDoc);
     }
 
 }
--- pages/categories.js
+++ pages/categories.js
@@ -2,31 +2,31 @@ import Layout from "@/components/Layout";
 import axios from "axios";
 import { useState } from "react";
 
+
 export default function Categories() {
-    const [name, setName] = useState([]);
-    function saveCategory(){
-        axios.post('/api/categories',{name});
+    // Define a state variable called "name" and a function called "setName" to update it
+    const [name, setName] = useState('');
+
+    // Define a function called "saveCategory" that is called when the form is submitted
+    async function saveCategory(ev){
+        // Prevent the default form submission behavior
+        ev.preventDefault();
+        // Send a POST request to the "/api/categories" endpoint with the "name" state variable as the request body
+        await axios.post('/api/categories',{name});
+        // Set the "name" state variable to an empty string
         setName('');
     }
-    return(
-        
-        <Layout>
-            <h1>
-                Categories
-            </h1>
-            <label>New Category Name</label>
-            <form onSubmit={saveCategory} className="flex gap-1">
-            <input 
-            className="my-0"
-            type="text"
-            placeholder={"Category Name"} 
-            onChange={(e) => setName(e.target.value)}
-            value={name}    
-            />
-            <button type="submit" className="btn-primary py-1">Submit</button>
-            </form>
-            
-        </Layout>
-    )
 
+    // Return a JSX element that renders a form to create new categories
+    return (
+        <form onSubmit={saveCategory}>
+            <label>
+                Name:
+                {/* Bind the input field to the "name" state variable and update it when the user types in the input field */}
+                <input type="text" value={name} onChange={e => setName(e.target.value)} />
+            </label>
+            {/* Trigger the form submission when the submit button is clicked */}
+            <button type="submit">Save</button>
+        </form>
+    );
 }
